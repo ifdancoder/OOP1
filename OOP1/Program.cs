35b0b1c7471db9572624e3feeda8379051ccdb79
ifdancoder@gmail.com <ifdancoder@gmail.com>
2022-02-20 18:58:10 +0800

    Добавьте файлы проекта.

---
+++ OOP1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("b: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("ax: ");
+        double ax = double.Parse(Console.ReadLine());
+        Console.Write("by: ");
+        double by = double.Parse(Console.ReadLine());
+        Console.WriteLine("Your answer is: " + (ax * a + b == by ? "YES" : "NO"));
+    }
+}
